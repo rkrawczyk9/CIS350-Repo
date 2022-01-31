baedb7ef71beb4297a0cfbf7d55e4dd8092a5d71
Robert Krawczyk <rkrawczyk@mail.bradley.edu>
2022-01-30 22:17:31 -0600

    Fixed the things

---
+++ Challenge1/Assets/Challenge 1/Scripts/PlayerScores.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Robert Krawczyk
+ * Challenge 1
+ * Player can score or die by entering trigger zones
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScores : MonoBehaviour
+{
+    public ScoreManager scoreManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Finish"))
+        {
+            scoreManager.Score();
+        }
+        else if (collision.gameObject.CompareTag("Death"))
+        {
+            scoreManager.Lose();
+        }
+    }
+}
---
+++ Challenge1/Assets/Challenge 1/Scripts/ScoreManager.cs
@@ -0,0 +1,65 @@
+﻿/*
+ * Robert Krawczyk
+ * Challenge 1
+ * Manages score and winning and losing
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScoreManager : MonoBehaviour
+{
+    public Text scoreText;
+    public Text winloseText;
+    public Text restartText;
+
+    public int score = 0;
+    private int winning_score = 5;
+    private bool ready_to_restart = false;
+
+    public void Score()
+    {
+        if (!ready_to_restart)
+        {
+            score++;
+            scoreText.text = score.ToString();
+            if (score >= winning_score)
+            {
+                winloseText.text = "You win!";
+                restartText.text = "Restart? [r]";
+                ready_to_restart = true;
+            }
+        }
+    }
+
+    public void Lose()
+    {
+        if (!ready_to_restart)
+        {
+            winloseText.text = "You lose!";
+            restartText.text = "Restart? [r]";
+            ready_to_restart = true;
+        }
+    }
+
+    void Start()
+    {
+        scoreText.text = "0";
+        winloseText.text = "";
+        restartText.text = "";
+    }
+
+    void Update()
+    {
+        if (ready_to_restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+}
