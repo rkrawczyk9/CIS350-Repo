c93bbb4b2f2d9697dc5b33c96e2543ed5041216b
Robert Krawczyk <rkrawczyk@mail.bradley.edu>
2022-05-08 00:23:59 -0500

    NavMesh stuff
    Unfortunately I couldn't get Titus to play animations correctly so I left it out

---
+++ Assignment 9/Assets/Finished01/Scripts/MoveToClickPoint.cs
@@ -0,0 +1,25 @@
+﻿// MoveToClickPoint.cs
+using UnityEngine;
+using UnityEngine.AI;
+public class MoveToClickPoint : MonoBehaviour
+{
+    public Camera cam;
+    public NavMeshAgent agent;
+    void Start()
+    {
+        cam = Camera.main;
+        agent = GetComponent<NavMeshAgent>();
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.destination = hit.point;
+            }
+        }
+    }
+}
---
+++ Assignment 9/Assets/Finished04/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+public class PlayerController : MonoBehaviour
+{
+    public Camera cam;
+    public NavMeshAgent agent;
+    public ThirdPersonCharacter character;
+    void Start()
+    {
+        cam = Camera.main;
+        character = GetComponent<ThirdPersonCharacter>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+
+        if(agent.remainingDistance > agent.stoppingDistance)
+        {
+            character.Move(agent.desiredVelocity, false, false);
+        }
+        else
+        {
+            character.Move(Vector3.zero, false, false);
+        }
+    }
+}
